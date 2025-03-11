f8498f00726361a584adbdcadc2e15aeffb076c8
jyx <yixin.24@intl.zju.edu.cn>
2025-03-11 04:16:52 +0100

    correct variable.py

--- Practical4/variable.py
+++ Practical4/variable.py
@@ -4,8 +4,12 @@ c = a + b # c is the total length of time of the bus commute
 d = 90 # the drive takes 1hr and 30 mins
 e = 5 # the driver needs 5 mins to walk to the final destination
 f = d + e # f is the total time of time of the car commute
-if c > e: # compare c with e to find out the shortest way of commute 
-    print(True) # if c > e, print True. The outcome is True, so C > e. So the car travel is faster.
+if c > f: # compare c with e to find out the shortest way of commute 
+    print(f"the car travel takes {f} mins,", "the car travel is faster") # if c > f, the car travel is faster.
+elif c < f: # compare c with e to find out the shortest way of commute 
+    print(f"the bus travel takes {c} mins,", "the bus travel is faster") # if c < f, the bus travel is faster.
+else: # compare c with e to find out the shortest way of commute
+    print("the two methods are the same") # if c = f, the two methods are the same.
 
 X = True # the value of X is True
 Y = False # the value of Y is False
