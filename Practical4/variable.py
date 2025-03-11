79c24060d64621466325303aa9c60ccdf2ead79d
jyx <yixin.24@intl.zju.edu.cn>
2025-03-11 03:22:06 +0100

    add Bool variable to variable.py

--- Practical4/variable.py
+++ Practical4/variable.py
@@ -6,3 +6,8 @@ e = 5
 f = d + e
 if c > e:
     print(True) # if c > e, print True. The outcome is True, so C > e
+
+X = True
+Y = False
+W = X and Y
+print(W) # print the outcome of W. Because X is True and Y is False, W (X and Y) is False.
