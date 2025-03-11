f82d63c57799a06fb6fbe4c188f0815610605bf9
jyx <yixin.24@intl.zju.edu.cn>
2025-03-11 04:01:31 +0100

    add comments in triangle.py

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,6 +1,6 @@
-sum = 0 
+sum = 0 # initialize the sum
 n = 1 # starting from the first triangle
-while n <= 10: # we need to count first 10 triangle, so n should be no more than 10
+while n <= 10: # we need to count first 10 triangles, so n should be no more than 10
     sum += n # sum can be calculated	as the sum of the n	numbers	from 1 to n
     print(sum) # we need to show the first ten values
     n += 1 # n should move on
