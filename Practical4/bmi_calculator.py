0d4710818239057603274e6809198bdf7a214122
jyx <yixin.24@intl.zju.edu.cn>
2025-03-11 03:50:42 +0100

    create bmi_calculator.py

---
+++ Practical4/bmi_calculator.py
@@ -0,0 +1,10 @@
+height = 163  # stating the person's height 
+weight = 55  # stating the person's weight
+BMI = weight/height**2  # using the formmula to calculate the person's BMI
+print("the person's BMI is:", "BMI")  # show the person's BMI
+if BMI > 30:  # compare the person's BMI with 30
+    print("the person is obese")  # if the BMI > 30, the person is obese
+elif BMI < 18.5:  # compare the person's BMI with 18.5
+    print("the person is underweight") # if the BMI < 18.5, the person is underweight
+else:  # the person's BMI is neither over 30 nor less than 18.5
+    print("the person is in the normal weight")  # show that the person is in the normal weight
