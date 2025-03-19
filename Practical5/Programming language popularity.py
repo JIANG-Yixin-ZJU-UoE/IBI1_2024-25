0fccb97f8c77e98b85e2b9aa578c64d2f536c7f9
jyx <yixin.24@intl.zju.edu.cn>
2025-03-19 07:15:52 +0100

    edit Programming language popularity.py

--- Practical5/Programming language popularity.py
+++ Practical5/Programming language popularity.py
@@ -19,10 +19,10 @@ print(code_info1) # print the new dictionary
 import numpy as np # import numpy and name it np
 import matplotlib.pyplot as plt # import matplotlib.pyplot and name it plt
 N = 5 # create 5 groups, which is in accordance with 5 programming language types
-percentage_of_users = (62.3, 52.9, 51, 51, 38.5) # store the y-axis value
+percentage_of_users = (code_info.values()) # store the y-axis value
 ind = np.arange(N) # the x locations for the groups 
 width = 0.3 # the width of the bars
-p1 = plt.bar(ind, percentage_of_users, width) # create a bar chart using the "bar" function in matplotlib.pyplot
+plt.bar(ind, percentage_of_users, width) # create a bar chart using the "bar" function in matplotlib.pyplot
 plt.ylabel("percentage of users") # define the name of the y-axis
 plt.title("percentage of users who use the top 5 programming languages globally") # define the name of the bar chart
 plt.xticks(ind,("Javascript", "HTML", "Python", "SQL", "TypeScript")) # define the x-axis label
