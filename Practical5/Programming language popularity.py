0a18ef4d8d5fff678b4f403148aef7670d3cac2d
jyx <yixin.24@intl.zju.edu.cn>
2025-03-19 07:19:16 +0100

    edit Programming language popularity.py

--- Practical5/Programming language popularity.py
+++ Practical5/Programming language popularity.py
@@ -12,22 +12,23 @@ for i in code_info.values(): # iterating all values in the dictionary
 
 print(code_info["HTML"]) # print a percentage of users of one particular programming language that can be modified
 
-code_info1 = code_info # create a new dictionary called code_info1 that is the same as code_info
+code_info1 = code_info.copy() # create a new dictionary called code_info1 that is the same as code_info
 code_info1["Python"] = 80 # alter the value of the key "Python"
 print(code_info1) # print the new dictionary
 
 import numpy as np # import numpy and name it np
 import matplotlib.pyplot as plt # import matplotlib.pyplot and name it plt
-N = 5 # create 5 groups, which is in accordance with 5 programming language types
+N = len(code_info) # create 5 groups, which is in accordance with 5 programming language types
 percentage_of_users = (code_info.values()) # store the y-axis value
 ind = np.arange(N) # the x locations for the groups 
 width = 0.3 # the width of the bars
 plt.bar(ind, percentage_of_users, width) # create a bar chart using the "bar" function in matplotlib.pyplot
 plt.ylabel("percentage of users") # define the name of the y-axis
 plt.title("percentage of users who use the top 5 programming languages globally") # define the name of the bar chart
-plt.xticks(ind,("Javascript", "HTML", "Python", "SQL", "TypeScript")) # define the x-axis label
+plt.xticks(ind,code_info.keys()) # define the x-axis label
 plt.yticks(np.arange(0, 70, 10)) # define the y-axis scale, ranging from 0 to 70, using 10 as a scale unit
 plt.show() # show the bar chart
- 
+
+
 
 
