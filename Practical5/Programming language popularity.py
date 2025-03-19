c703ddbb50d670c6e3b2e3f6a07e398a9259c837
jyx <yixin.24@intl.zju.edu.cn>
2025-03-19 06:43:44 +0100

    add loops and pseudocode to the file

--- Practical5/Programming language popularity.py
+++ Practical5/Programming language popularity.py
@@ -1,10 +1,20 @@
+# Program: to explore the popularity of different programming languages
+# Procedures:
 # 1.to create a dictionary to store programming language popularity
-# 2.print a percentage of users of one programming language that can be modified
+# 2.print percentages of users of all programming languages that can be modified (after which make some changes)
 # 3.draw a bar graph using numpy and matplotlib.pyplot
 
 code_info = {"Javascript": 62.3, "HTML": 52.9, "Python": 51, "SQL": 51, "TypeScript": 38.5} # create a dictionary to store programming language popularity
 print(code_info) # print the dictionary
-print(code_info["HTML"]) # print a percentage of users of one programming language that can be modified
+
+for i in code_info.values(): # iterating all values in the dictionary
+    print(i) # print them out
+
+print(code_info["HTML"]) # print a percentage of users of one particular programming language that can be modified
+
+code_info1 = code_info # create a new dictionary called code_info1 that is the same as code_info
+code_info1["Python"] = 80 # alter the value of the key "Python"
+print(code_info1) # print the new dictionary
 
 import numpy as np # import numpy and name it np
 import matplotlib.pyplot as plt # import matplotlib.pyplot and name it plt
