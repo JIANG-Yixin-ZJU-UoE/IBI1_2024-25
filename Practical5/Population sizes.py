9b7462299094bb85340718f25b0361415839a480
jyx <yixin.24@intl.zju.edu.cn>
2025-03-19 08:10:06 +0100

    add lists in the file

--- Practical5/Population sizes.py
+++ Practical5/Population sizes.py
@@ -1,12 +1,16 @@
 # store and show the population in UK countries and Zhejiang-neighboring provinces
+# Procedures:
 # 1.create two lists to store infomation about the population and sort them
 # 2.draw a pie chart for population in uk
 # 3.draw a pie chart for population in provinces in China
 
-uk_countries_population = {"England": 57.11, "Wales": 3.13, "Norhtern Ireland": 1.91, "Scotland": 5.45} # store population infomation in uk
-china_countries_population = {"Zhejiang": 65.77, "Fujian": 41.88, "Jiangxi": 45.28, "Anhui": 61.27, "Jiangsu": 85.15} # store population infomation in Zhejiang-neighboring provinces
-print(sorted(uk_countries_population.values())) # print sorted list of population in uk
-print(sorted(china_countries_population.values())) # print sorted list of population in Zhejiang-neighboring provinces
+uk_popu = [57.11, 3.13, 1.91, 5.45] # store population infomation in uk
+china_popu = [65.77, 41.88, 45.28, 61.27, 85.15] # store population infomation in Zhejiang-neighboring provinces
+print(sorted(uk_popu)) # print sorted list of population in uk
+print(sorted(china_popu)) # print sorted list of population in Zhejiang-neighboring provinces
+
+uk_countries_population = {"England": 57.11, "Wales": 3.13, "Norhtern Ireland": 1.91, "Scotland": 5.45} # store population infomation in uk as dictionary
+china_countries_population = {"Zhejiang": 65.77, "Fujian": 41.88, "Jiangxi": 45.28, "Anhui": 61.27, "Jiangsu": 85.15} # store population infomation in Zhejiang-neighboring provinces as dictionary
 
 import matplotlib.pyplot as plt # import matplotlib.pyplot and name it plt
 plt.figure() # show two pictures at the same time
