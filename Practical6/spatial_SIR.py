fe1a393a710894a417ca01669f4bf35d12fdca9c
jyx <yixin.24@intl.zju.edu.cn>
2025-03-27 13:08:28 +0100

    change spatial_SIR.py

--- Practical6/spatial_SIR.py
+++ Practical6/spatial_SIR.py
@@ -1,63 +1,53 @@
+# goal: Looking at disease spread in 2D
+# procedures:
+# 1. import necessary libraries
+# 2. choose a random point in an array
+# 3. deal with how to infect neighbors
+# 4. deal with the recovery
+# 5. plot four heat diagrams
+
+
 # import necessary libraries
 import numpy as np
 import matplotlib. pyplot as plt
+import random
 
 # make array of all susceptible population
-population = np.zeros( ( 100 , 100 ) )
-outbreak = np.random.choice( range(100) , 2 )
-population[ outbreak[ 0 ] , outbreak[ 1 ] ] = 1
-plt.figure( figsize = ( 6 , 4 ) , dpi =150)
-plt.imshow( population , cmap= 'viridis', interpolation= 'nearest')
+population = np.zeros((100, 100))
+outbreak = np.random.choice(range(100), 2)
+population[outbreak[0], outbreak[1]] = 1
+
 
 timesteps = 100
 I = 1
 beta = 0.3
 gamma = 0.05
 
+# check the neighbers' infection(spread of the disease)
+for i in range(100):
+    for j in range(100):
+        if population[i, j] == 1:
+            # check the 8 neighbors
+            for di in [-1, 0, 1]:
+                for dj in [-1, 0, 1]:
+                    if 0 <= i+di < 100 and 0 <= j+dj < 100:
+                        if population[i+di, j+dj] == 0:
+                            if random.random() < beta:
+                                population[i+di, j+dj] = 1
 
-# define the color
-color_mapping = {
-    0: [0.18, 0.80, 0.44, 1], 
-    1: [0.91, 0.30, 0.24, 1], 
-    2: [0.20, 0.60, 0.86, 1]  
-}
-def generate_colors(grid):
-    return np.array([[color_mapping[val] for val in row] for row in grid])
+# recovery
+infected = []
+for people in population:
+    if population[i, j] == 1:       
+        infected.append([i, j])
+        for di, dj in infected:
+            if np.random.random() < gamma:
+                population[i, j] = 2
 
-# draw the plot
-img = plt.imshow(generate_colors(population))
-plt.axis('off')
-plt.title(f"SIR Spatial Spread - Step 0 (Initial Clusters: {I})")
-
-# renew the loop
-for step in range(timesteps):
-    new_pop = population.copy()
-    
-    # recovery
-    infected = np.argwhere(population == 1)
-    for i, j in infected:
-        if np.random.rand() < gamma:
-            new_pop[i, j] = 2
-    
-    # check the neighbers' infection(spread of the disease)
-    for i in range(100):
-        for j in range(100):
-            if population[i, j] == 1:
-                # check the 8 neighbors
-                for di in [-1, 0, 1]:
-                    for dj in [-1, 0, 1]:
-                        if 0 <= i+di < 100 and 0 <= j+dj < 100:
-                            if new_pop[i+di, j+dj] == 0:
-                                if np.random.rand() < beta:
-                                    new_pop[i+di, j+dj] = 1
-    
-    population = new_pop.copy()
-    img.set_data(generate_colors(population))
-    plt.title(f"SIR Spatial Spread - Step {step+1}")
-    plt.draw()
-    plt.pause(0.2)
-    
-    if not plt.get_fignums():
-        break
-
-plt.show()
+for i in [0, 10, 50, 100]:
+    plt.figure(figsize = (6, 4), dpi =150)
+    plt.imshow(population, cmap= 'viridis', interpolation= 'nearest')
+    plt.plot(population = 0, color = "purple")
+    plt.plot(population = 1, color = "green")
+    plt.plot(population = 2, color = "yellow")
+    plt.show()
