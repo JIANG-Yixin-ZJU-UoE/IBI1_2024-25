83b12fc5ab18da7e59101e517142ac24bd79af36
jyx <yixin.24@intl.zju.edu.cn>
2025-03-28 05:23:48 +0100

    add pseudocode

--- Practical6/SIR_vaccination.py
+++ Practical6/SIR_vaccination.py
@@ -1,9 +1,17 @@
-# import necessary libraries
-import numpy as np
-import matplotlib.pyplot as plt
+# goal: to look at the effect of the vaccination
+# procedures:
+# 1. import necessary libraries
+# 2. set initial parameters
+# 3. set the time loop to pick susceptible people at random to be infected, pick infected people at random to be recovered and record every loop of the infected
+# 4. draw a figure depicting the graph
 
-plt.figure(figsize=(6, 4), dpi=150)
 
+import numpy as np # import numpy
+import matplotlib.pyplot as plt # import matplotlib.pyplot
+
+plt.figure(figsize=(6, 4), dpi=150) # determine the dimensions and resolution
+
+# set the initial parameters and set a loop to iterate all possible vaccination rates from 0% to 100%
 for i in range(0,110,10):
     N = 10000
     vacc_rate = i/100
@@ -15,35 +23,29 @@ for i in range(0,110,10):
     gamma = 0.05
     I_list = [I]
 
-    # time loop:
-    # 1. pick susceptible people at random to be infected
-    # 2. pick infected people at random to be recovered
-    # 3. record every loop in the according list
-    time = 0
-    while time <= 1000:
-                 
-        L1 = np.random.choice(range(0,2), S, p=[1-beta*I/N, beta*I/N])
-        new_I = sum(L1)
-        S = max(S - new_I, 0)  
-        L2 = np.random.choice(range(0,2), I, p=[1-gamma, gamma])
-        new_R = sum(L2)
-        I = min( (I + new_I - new_R),10000)
-        R = R + new_R 
-        I_list.append(I)
-        time += 1
 
-# draw a figure depicting the graph
-# 1. plot the three curves S,I,R, using different colors
-# 2. set x/y axis parameters
-    plt.plot(I_list, label=f'Vaccination {i}%')
+    time = 0
+    while time < 1000: # loop 1000 times    
+        L1 = np.random.choice(range(0,2), S, p=[1-beta*I/N, beta*I/N]) # pick susceptible people at random to be infected
+        new_I = sum(L1) # calculate the number of people infected
+        S = max(S - new_I, 0)  # to record the S value and ensure it is bigger than 0
+        L2 = np.random.choice(range(0,2), I, p=[1-gamma, gamma]) # pick infected people at random to be recovered
+        new_R = sum(L2) # calculate the number of people recovered
+        I = min( (I + new_I - new_R),10000) # to record I value and ensure it is smaller than 10000
+        R = R + new_R # renew the R value
+        I_list.append(I) # store the I value into the list
+        time += 1 # plus one to enter another loop
+
+
+    plt.plot(I_list, label=f'{i}%') # set the label/legend for the plot
    
 
-plt.xlabel('Time')
-plt.xticks(range(0,1200,200), ('0', '200', '400', '600', '800', '1000'))
-plt.ylabel('Number of People')
-plt.title('Basic SIR Model Simulation with different vaccination rate')
-plt.legend(loc='upper right')
-plt.show()
+plt.xlabel('Time') # set the x label
+plt.xticks(range(0,1200,200), ('0', '200', '400', '600', '800', '1000')) # set the x ticks from 0 to 1000
+plt.ylabel('Number of People') # set the y label
+plt.title('Basic SIR Model Simulation with different vaccination rate') # set the title of the graph
+plt.legend(loc='upper right') # set the legend
+plt.show() # show the graph
 
 
     
