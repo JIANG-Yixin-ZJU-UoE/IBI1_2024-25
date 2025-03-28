8bd739b9839eb8416bc275c488e1e60f149923ad
jyx <yixin.24@intl.zju.edu.cn>
2025-03-28 05:44:06 +0100

    add pseudocode

--- Practical6/spatial_SIR.py
+++ Practical6/spatial_SIR.py
@@ -6,71 +6,66 @@
 # 4. deal with the recovery
 # 5. plot four heat diagrams
 
-
-# import necessary libraries
-import numpy as np
-import matplotlib. pyplot as plt
-import random
+import numpy as np # import numpy
+import matplotlib. pyplot as plt # import matplotlib.pyplot
+import random # import random
 
 # make array of all susceptible population
-population = np.zeros((100, 100))
-outbreak = np.random.choice(range(100), 2)
-population[outbreak[0], outbreak[1]] = 1
+population = np.zeros((100, 100)) # make a 100*100 matrix with all zeros
+outbreak = np.random.choice(range(100), 2) # randomly choose two elements (the x and y coordinates)
+population[outbreak[0], outbreak[1]] = 1 # define the infected coordinates
 
-timesteps = 100
+# set the initial parameters
 I = 1
 beta = 0.3
 gamma = 0.05
 
-
+# infect the neighbors and simulate the process of one person infecting others at a time
 def step(population):
-    infected = []
-    copy = population.copy()
+    infected = [] # store the infected information
+    copy = population.copy() # copy the population to make the change, or the newly infected people will affect others in one step
     for i in range(100):
-        for j in range(100):
-            if population[i, j] == 1:
+        for j in range(100): # iterate all members in the matrix
+            if population[i, j] == 1: # find the infected
                 # check the 8 neighbors
                 for di in [-1, 0, 1]:
-                    for dj in [-1, 0, 1]:
-                        if 0 <= i+di < 100 and 0 <= j+dj < 100:
-                            if population[i+di, j+dj] == 0:
-                                if random.random() < beta:
-                                    copy[i+di, j+dj] = 1
+                    for dj in [-1, 0, 1]: # iterate the neighbors
+                        if 0 <= i+di < 100 and 0 <= j+dj < 100: # make sure the coordinates are in the matrix
+                            if population[i+di, j+dj] == 0: # find the neighbors that are not infected
+                                if random.random() < beta: 
+                                    copy[i+di, j+dj] = 1 # the neighbor has beta percent of being infected
 
-                # recovery
-                #1记录所有的感染者2对感染者执行概率康复
-
-                infected.append([i, j])
+                infected.append([i, j]) # add infected coordinates
+    # iterate the infected to make possible reocvery
     for i, j in infected:
         if np.random.random() < gamma:
-            copy[i, j] = 2
+            copy[i, j] = 2 # the infected have gamma percent of recovery
     
     return copy
 
+fig = plt.figure(figsize= (6, 4), dpi= 150) # determine the dimensions and resolution
+ax = fig.add_subplot(221) # draw a subplot in the first place of a whole plot
+ax2 = fig.add_subplot(222) # draw a subplot in the second place of a whole plot
+ax3 = fig.add_subplot(223) # draw a subplot in the third place of a whole plot
+ax4 = fig.add_subplot(224) # draw a subplot in the forth place of a whole plot
 
-fig = plt.figure(figsize = (6, 4), dpi =150)
-ax = fig.add_subplot(221)
-ax2 = fig.add_subplot(222)
-ax3 = fig.add_subplot(223)
-ax4 = fig.add_subplot(224)
 
-
-ax.imshow(population, cmap= 'viridis', interpolation= 'nearest')
-for i in range(10):
-    population = step(population)
-ax2.imshow(population, cmap= 'viridis', interpolation= 'nearest')
+ax.imshow(population, cmap= 'viridis', interpolation= 'nearest') # draw the initial plot with one randomly choosed point
+for i in range(10): 
+    population = step(population) # use the step function 10 times  
+ax2.imshow(population, cmap= 'viridis', interpolation= 'nearest') # draw the plot
 for i in range(40):
-    population = step(population)
-ax3.imshow(population, cmap= 'viridis', interpolation= 'nearest')
+    population = step(population) # use the function another 40 times 
+ax3.imshow(population, cmap= 'viridis', interpolation= 'nearest') # draw the plot
 for i in range(50):
-    population = step(population)
-ax4.imshow(population, cmap= 'viridis', interpolation= 'nearest')
+    population = step(population) # use the function another 50 times
+ax4.imshow(population, cmap= 'viridis', interpolation= 'nearest') # draw the plot
 
 for ax in [ax, ax2, ax3, ax4]:
-    ax.set_xticks(range(0, 100, 20), ("0", "20", "40", "60", "80"))
-    ax.set_yticks(range(80 ,-20, -20), ('80', '60', '40', '20', '0'))
-    ax.plot(population = 0, color = "purple")
-    ax.plot(population = 1, color = "green")
-    ax.plot(population = 2, color = "yellow")
+    ax.set_xticks(range(0, 100, 20), ("0", "20", "40", "60", "80")) # set the x ticks from 0 to 80
+    ax.set_yticks(range(80 ,-20, -20), ('80', '60', '40', '20', '0')) # set the y ticks from 80 to 0
+    ax.plot(population = 0, color = "purple") # make the susceptible points purple
+    ax.plot(population = 1, color = "green") # make the infected points green
+    ax.plot(population = 2, color = "yellow") # make the recovered points yellow
 
-plt.show()
+plt.show() # show the plot
