aeb7bfc66fdd2c1a6ca8687c2c272a6fd2aeebb8
jyx <yixin.24@intl.zju.edu.cn>
2025-04-02 07:03:57 +0100

    refine tata_box.py

---
+++ Practical7/spliced_tata_genes.py
@@ -0,0 +1,1 @@
+combination = input("please choose one from the three sequence: GTAG, GCAG, ATAC")
--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -1,12 +1,16 @@
-input = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
+input = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r') 
 str = ""
+gene_name = []
 import re
 for line in input:
+    line = line.strip()
     if re.search(r'gene:(\S+)\s', line):
-        if re.search('TATA', str):
-            #str = 'CGCGCGCGCGCGCGCGGCGCATATTATATATATAT'
-            print(gene_name[0])
-            print(str)
+        if re.search(r'TATA[AT]A[AT]', str):
+            # print('>' + gene_name[0])
+            # print(str)
+            out_file = open('tata_genes.fa', 'a')
+            out_file.write('>' + gene_name[0] + '\n')
+            out_file.write(str + '\n')
             gene_name = re.findall(r'gene:(\S+)\s', line)
             str = ''
         else:
@@ -14,3 +18,5 @@ for line in input:
             str = ''
     else:
         str += line.strip()
+
+
