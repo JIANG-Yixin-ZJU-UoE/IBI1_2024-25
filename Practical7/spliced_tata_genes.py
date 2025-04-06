2b93f7b39ae979d2d68c5b928e40823d2d369d73
jyx <yixin.24@intl.zju.edu.cn>
2025-04-06 07:38:44 +0100

    add pseudocode

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -5,8 +5,9 @@
 # 3. judge if the sequence contains TATA
 # 4. if so, store the sequence in a fa file, count the instances of TATA and print the gene name
 
-import re
-combination = input("please choose one from the three sequence: GTAG, GCAG, ATAC: ")
+import re # import the re to use later
+combination = input("please choose one from the three sequence: GTAG, GCAG, ATAC: ") # ask the user to input
+# open the file and initialize the list, str, gene name and TATA count
 list = ['GTAG', 'GCAG', 'ATAC']
 raw_file = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
 str = ""
@@ -15,23 +16,23 @@ TATA_count = 0
 
 for i in range(0,3):
     if combination == list[i]:
-        out_file = open(f'{list[i]}_spliced_genes.fa', 'w')
+        out_file = open(f'{list[i]}_spliced_genes.fa', 'w') # create according file
         for line in raw_file:
-            if re.search(r'gene:(\S+)\s', line):
-                if re.search(rf'{list[i][0:2]}.+{list[i][2:4]}', str):
+            if re.search(r'gene:(\S+)\s', line): # judge if the line contains the gene name
+                if re.search(rf'{list[i][0:2]}.+{list[i][2:4]}', str): # judge if the last sequence contains donor/acceptor
                     # count the instances of TATAWAW
-                    instance_list = re.findall(r'TATA[AT]A[AT]', str)
-                    TATA_count = len(instance_list)
-                    out_file = open(f'{list[i]}_spliced_genes.fa', 'a')
-                    out_file.write('>' + gene_name + f'   sequence TATAWAW appears {TATA_count} times' '\n')
-                    out_file.write(str + '\n')
-                    gene_name = re.findall(r'gene:(\S+)\s', line)[0]
-                    str = ''
+                    instance_list = re.findall(r'TATA[AT]A[AT]', str) # find all the TATA boxes
+                    TATA_count = len(instance_list) # count the number of TATA instances
+                    out_file = open(f'{list[i]}_spliced_genes.fa', 'a') # open the according file
+                    out_file.write('>' + gene_name + f'   sequence TATAWAW appears {TATA_count} times' '\n') # write the gene name and the TATA count
+                    out_file.write(str + '\n') # write the sequence
+                    gene_name = re.findall(r'gene:(\S+)\s', line)[0] # define the gene name as the next gene
+                    str = '' # reinitialize the str
                 else:
-                    gene_name = re.findall(r'gene:(\S+)\s', line)[0]
-                    str = ''
+                    gene_name = re.findall(r'gene:(\S+)\s', line)[0] # define the gene name as the next gene
+                    str = '' # reinitialize the str
             else:
-                str += line.strip()
+                str += line.strip() # add the line into the str
 
 
 
