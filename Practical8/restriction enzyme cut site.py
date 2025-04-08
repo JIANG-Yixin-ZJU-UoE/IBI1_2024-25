c165167f65927003fb971cae8eb96b3b0a2dc625
jyx <yixin.24@intl.zju.edu.cn>
2025-04-08 05:17:41 +0100

    modify the logic mistake

--- Practical8/restriction enzyme cut site.py
+++ Practical8/restriction enzyme cut site.py
@@ -1,7 +1,7 @@
 import re
-cut_sequence = "ACGTCGTGACTCGATCGATC"
-recognized_sequence = "TCGT"
-def site(cut_sequence, recognized_sequence):
+cut_sequence = "ACGTCGTGACTCGTATCGATC"
+recog_seq = "TCGT"
+def site(cut_sequence, recog_seq):
     flag = False
     for i in cut_sequence:
         if i == "A" or i == "T" or i == "G" or i == "C":
@@ -12,12 +12,14 @@ def site(cut_sequence, recognized_sequence):
             return seq_error
 
     if flag == False:
-        if re.findall(rf"{recognized_sequence}", cut_sequence):
+        if re.findall(rf"{recog_seq}", cut_sequence):
             index = 1
-            for i in cut_sequence:
-                if i == recognized_sequence[0]:
-                    return index, recognized_sequence[0]
+            for i in range(len(cut_sequence)-3):
+                test_str = cut_sequence[i:i+len(recog_seq)]
+                if test_str == recog_seq:
+                    print(index, recog_seq[0])
+                    break                 
                 else:
                     index += 1
 
-print(site(cut_sequence, recognized_sequence))
+print(site(cut_sequence, recog_seq))
