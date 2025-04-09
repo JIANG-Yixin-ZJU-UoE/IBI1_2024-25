533c596811b676afa95a27a97d236818ae5aaab8
jyx <yixin.24@intl.zju.edu.cn>
2025-04-09 06:31:33 +0100

    edit the code

--- Practical8/drug dosage calculator.py
+++ Practical8/drug dosage calculator.py
@@ -1,3 +1,12 @@
+# goal: to calculate needed drug dosage according to the person
+# procedure:
+# 1. obtain the weight and strength
+# 2. define a function called dosage with two parameters
+# 3. tell if the weight is in range and the strength is 120 or 250
+# 4. calculate the volume needed
+# 5. if the weight or the strength do not meet the requirements, print out the error
+# 6. use the function
+
 weight = float(input('please enter your weight: '))
 strength = float(input('please choose one strength from 120mg/5ml and 250mg/5ml: '))
 def dosage(weight, strength):
