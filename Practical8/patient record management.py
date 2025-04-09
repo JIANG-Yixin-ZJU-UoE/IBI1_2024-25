26b7016a1cdcf5dafdfa3cb0840690d64039c600
jyx <yixin.24@intl.zju.edu.cn>
2025-04-09 06:47:53 +0100

    adding comments

--- Practical8/patient record management.py
+++ Practical8/patient record management.py
@@ -1,12 +1,19 @@
+# goal: to create a class called patients to store people information
+# 1. create the class
+# 2. use init to initialize data attributes
+# 3. use a function to print out the patient's information
+# 4. make an example to use the class
+
+
 class patients:
-    def __init__(self, name, age, date_of_latest_admission, mediacal_history):
+    def __init__(self, name, age, date_of_latest_admission, mediacal_history): # initialize data attributes
         self.name = name
         self.age = age
         self.date_of_latest_admission = date_of_latest_admission
         self.mediacal_history = mediacal_history
-    def record(name, age, date_of_latest_admission, mediacal_history):
+    def record(name, age, date_of_latest_admission, mediacal_history): # print out the patient's information
         patients_record = f"{name}, {age}, {date_of_latest_admission}, {mediacal_history}"
-        print(patients_record)
         return patients_record
     
-print(patients.record("kate","32", "2024.11.23", "none"))
+# this is an example of using the class and the function to print out patients' information
+print(patients.record("kate", "32", "2024.11.23", "none"))
