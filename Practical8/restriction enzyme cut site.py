f5e25e972ed4f8d3fc4c791a0ad269aaaf5d58c8
jyx <yixin.24@intl.zju.edu.cn>
2025-04-09 07:14:23 +0100

    edit the code

--- Practical8/restriction enzyme cut site.py
+++ Practical8/restriction enzyme cut site.py
@@ -1,25 +1,33 @@
+# goal: to find the position of the first nucleotide in the cut sequence that matches the recognized sequence
+# procedure:
+# 1. set the initial sequence
+# 2. test whether all the inputs are within "A", "G", "C", "T"
+# 3. if it is a valid sequence, then find in the cut sequence the existence of recognized sequence
+# 4. if so, return the position and the nuleotide
+
 import re
-cut_sequence = "ACGTCGTGACTCGTATCGATC"
+cut_seq = "ACGTCGTGACTCGTATCGATC"
 recog_seq = "TCGT"
-def site(cut_sequence, recog_seq):
-    flag = False
-    for i in cut_sequence:
-        if i == "A" or i == "T" or i == "G" or i == "C":
-            flag = False
-        else:
-            flag = True
-            seq_error = "error: the DNA sequence is not valid"
-            return seq_error
+def site(cut_seq, recog_seq):
 
-    if flag == False:
-        if re.findall(rf"{recog_seq}", cut_sequence):
-            index = 1
-            for i in range(len(cut_sequence)-3):
-                test_str = cut_sequence[i:i+len(recog_seq)]
-                if test_str == recog_seq:
-                    print(index, recog_seq[0])
-                    break                 
-                else:
-                    index += 1
+    """ find out if all the inputs are "A", "G", "C", "T" """
 
-print(site(cut_sequence, recog_seq))
+    if not re.search(r'^[AGCT]+$', cut_seq):
+        print("the cut sequence is not valid")
+        exit()
+    if not re.search(r'^[AGCT]+$', recog_seq):
+        print("the recognized sequence is not valid")
+        exit()
+
+    list = []
+    if re.findall(rf"{recog_seq}", cut_seq):
+        index = 1
+        for i in range(len(cut_seq)-3):
+            test_str = cut_seq[i:i+len(recog_seq)]
+            if test_str == recog_seq:
+                list.append(index)       
+            else:
+                index += 1
+        return list
+
+print(site(cut_seq = "ACGTCGTGACTCGTATCGATC", recog_seq = "TCGT"))
