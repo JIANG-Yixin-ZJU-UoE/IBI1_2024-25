da2b89f5fd2c01e4556e7507788fb8eb2c96b473
jyx <yixin.24@intl.zju.edu.cn>
2025-04-09 06:52:20 +0100

    edit the code

--- Practical8/patient record management.py
+++ Practical8/patient record management.py
@@ -11,7 +11,8 @@ class patients:
         self.age = age
         self.date_of_latest_admission = date_of_latest_admission
         self.mediacal_history = mediacal_history
-    def record(name, age, date_of_latest_admission, mediacal_history): # print out the patient's information
+    
+    def record(self, name, age, date_of_latest_admission, mediacal_history): # print out the patient's information
         patients_record = f"{name}, {age}, {date_of_latest_admission}, {mediacal_history}"
         return patients_record
     
