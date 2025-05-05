696594c08044c18f3b23890b7ed8176f5a790bbf
jyx <yixin.24@intl.zju.edu.cn>
2025-05-05 14:07:25 +0100

    change errors in the pseudocode

--- Practical4/variable.py
+++ Practical4/variable.py
@@ -1,8 +1,8 @@
-a = 15 # The walk to the bus stop is 15	mins
-b = 75 # The bus journey takes 1 hr and	15 mins
+a = 15 # The walk to the bus stop is 15	minutes
+b = 75 # The bus journey takes 1 hour and 15 minutes
 c = a + b # c is the total length of time of the bus commute
-d = 90 # the drive takes 1hr and 30 mins
-e = 5 # the driver needs 5 mins to walk to the final destination
+d = 90 # the drive takes 1 hour and 30 minutes
+e = 5 # the driver needs 5 minutes to walk to the final destination
 f = d + e # f is the total time of time of the car commute
 if c > f: # compare c with e to find out the shortest way of commute 
     print(f"the car travel takes {f} mins,", "the car travel is faster") # if c > f, the car travel is faster.
@@ -10,7 +10,7 @@ elif c < f: # compare c with e to find out the shortest way of commute
     print(f"the bus travel takes {c} mins,", "the bus travel is faster") # if c < f, the bus travel is faster.
 else: # compare c with e to find out the shortest way of commute
     print("the two methods are the same") # if c = f, the two methods are the same.
-# The bus ride is quiker and needs shorter time.
+# The bus ride is quicker and needs shorter time.
 
 X = True # the value of X is True
 Y = False # the value of Y is False
