70b44ea194c68375ad31b4f10c61a9d7dcc47b98
jyx <yixin.24@intl.zju.edu.cn>
2025-05-06 03:52:57 +0100

    revise the code

--- Practical10/dalys.py
+++ Practical10/dalys.py
@@ -17,7 +17,7 @@ dalys_data = pd.read_csv("dalys-rate-from-all-causes.csv") # import the .csv fil
 
 # showing the third column (the year) for the first 10 rows (inclusive)
 column = [False, False, True, False]
-print(dalys_data.iloc[0:10, column]) # the 10th year of the recording is 1999
+print(f"{dalys_data.iloc[0:10, column]}\nthe 10th year of the recording is 1999") # the 10th year of the recording is 1999
 
 # used a Boolean to show DALYs for all countries in 1990
 year_Boolean_1990 = []
@@ -35,21 +35,23 @@ print(dalys_data.loc[row.index, ["Entity", "Code", "Year", "DALYs"]])
 # compute the mean DALYs in the UK and France, it turn out that the mean DALYs in the UK was greater than France
 print(dalys_data.loc[dalys_data.Entity == "United Kingdom", "DALYs"].describe()["mean"])
 print(dalys_data.loc[dalys_data.Entity == "France", "DALYs"].describe()["mean"])
+print("the mean DALYs in the UK was greater than France")
 
 # create a graph showing the DALYS changes over time in the UK
 uk = dalys_data.loc[dalys_data.Entity == "United Kingdom", ["DALYs", "Year"]]
-plt.figure(figsize = (8, 6))
+plt.figure(figsize = (14, 6))
+plt.subplot(1, 2, 1)
 plt.title("the DALYS changes over time in the UK")
 plt.plot(uk.Year, uk.DALYs, 'b+')
 plt.xticks(uk.Year, rotation = -90)
 plt.xlabel("Years")
 plt.ylabel("DALYs in the UK")
-plt.show()
 
 # draw a boxplot of DALYs across countries in 1990 (answer the question stated in file question.txt)
 DALYs_1990 = dalys_data.loc[dalys_data["Year"] == 1990, "DALYs"]
 data_list = DALYs_1990.tolist()
 n = 228
+plt.subplot(1, 2, 2)
 plt.title("DALYs across countries in 1990")
 plt.xlabel("228 countries in 1990")
 plt.ylabel("DALYs across countries")
