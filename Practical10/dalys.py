8947fc10a9022d1433cfefa2fb1d5af6db3407bd
jyx <yixin.24@intl.zju.edu.cn>
2025-05-06 04:06:17 +0100

    revise the code and the file questions

--- Practical10/dalys.py
+++ Practical10/dalys.py
@@ -39,19 +39,18 @@ print("the mean DALYs in the UK was greater than France")
 
 # create a graph showing the DALYS changes over time in the UK
 uk = dalys_data.loc[dalys_data.Entity == "United Kingdom", ["DALYs", "Year"]]
-plt.figure(figsize = (14, 6))
-plt.subplot(1, 2, 1)
+plt.figure(figsize = (8, 6))
 plt.title("the DALYS changes over time in the UK")
 plt.plot(uk.Year, uk.DALYs, 'b+')
 plt.xticks(uk.Year, rotation = -90)
 plt.xlabel("Years")
 plt.ylabel("DALYs in the UK")
+plt.show()
 
 # draw a boxplot of DALYs across countries in 1990 (answer the question stated in file question.txt)
 DALYs_1990 = dalys_data.loc[dalys_data["Year"] == 1990, "DALYs"]
 data_list = DALYs_1990.tolist()
 n = 228
-plt.subplot(1, 2, 2)
 plt.title("DALYs across countries in 1990")
 plt.xlabel("228 countries in 1990")
 plt.ylabel("DALYs across countries")
