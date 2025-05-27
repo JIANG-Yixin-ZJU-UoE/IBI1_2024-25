3e39a09e1d60263e8d3cd57f2fe7d925e30e68f2
Yixin Jiang <yixin.24@intl.zju.edu.cn>
2025-05-27 22:44:55 +0800

    revise the code in tata_box.py

--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -6,7 +6,7 @@
 # 4. if the sequence contains TATA, create a file and store the gene name and the sequence into the file, reinitialize the list and the string
 # 5. if not, just reinitialize the list and the string
 
-input_file = open('Practical7/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r') # read the original file
+input_file = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r') # read the original file
 store_str = "" # initialize a string and a list
 gene_name = []
 import re # import the re to use later
@@ -14,7 +14,7 @@ for line in input_file: # iterate the line in the file
     line = line.strip() # link all lines together
     if re.search(r'gene:(\S+)\s', line): # judge if the line contains the gene name
         if re.search(r'TATA[AT]A[AT]', store_str): # judge if the last gene sequence contains TATA
-            out_file = open('Practical7/tata_genes.fa', 'a') # add contents in the file
+            out_file = open('tata_genes.fa', 'a') # add contents in the file
             out_file.write('>' + gene_name[0] + '\n') # write the gene name
             out_file.write(store_str + '\n') # write the sequence of the last gene
             gene_name = re.findall(r'gene:(\S+)\s', line) # define the gene name as the next gene
